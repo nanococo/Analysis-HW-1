1c59a5a3f24a5f305eb39390d41807c46b9574ff
JorgeGuty <49891177+JorgeGuty@users.noreply.github.com>
2020-02-24 13:02:12 -0600

    First commit

---
+++ Assets/Scripts/BubbleSort.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSort : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void sort(int[] arr)
+    {
+
+        int temp;
+        for (int j = 0; j <= arr.Length - 2; j++)
+        {
+            for (int i = 0; i <= arr.Length - 2; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    temp = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/QuickSort.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickSort : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Quick_Sort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int pivot = Partition(arr, left, right);
+
+            if (pivot > 1)
+            {
+                Quick_Sort(arr, left, pivot - 1);
+            }
+            if (pivot + 1 < right)
+            {
+                Quick_Sort(arr, pivot + 1, right);
+            }
+        }
+
+    }
+
+    private int Partition(int[] arr, int left, int right)
+    {
+        int pivot = arr[left];
+        while (true)
+        {
+
+            while (arr[left] < pivot)
+            {
+                left++;
+            }
+
+            while (arr[right] > pivot)
+            {
+                right--;
+            }
+
+            if (left < right)
+            {
+                if (arr[left] == arr[right]) return right;
+
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+
+            }
+            else
+            {
+                return right;
+            }
+        }
+    }
+
+}
